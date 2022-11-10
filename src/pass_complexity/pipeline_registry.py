d5a66c1e6cb31e9a592034fa48a06e9271d18f87
pacificus <masterkristall@gmail.com>
2022-11-10 20:26:20 +0700

    Remove api_inference pipeline

--- src/pass_complexity/pipeline_registry.py
+++ src/pass_complexity/pipeline_registry.py
@@ -5,7 +5,7 @@ from kedro.pipeline import Pipeline
 from pass_complexity.pipelines.data_processing import pipeline as de
 from pass_complexity.pipelines.data_science import pipeline as ds
 from pass_complexity.pipelines.inference import pipeline as inference
-from pass_complexity.pipelines.api_inference import pipeline as api_inference
+#from pass_complexity.pipelines.api_inference import pipeline as api_inference
 
 
 def register_pipelines() -> Dict[str, Pipeline]:
@@ -18,7 +18,7 @@ def register_pipelines() -> Dict[str, Pipeline]:
     data_processing_pipeline = de.create_pipeline()
     data_science_pipeline = ds.create_pipeline()
     inference_pipeline = inference.create_pipeline()
-    api_inference_pipeline = api_inference.create_pipeline()
+    #api_inference_pipeline = api_inference.create_pipeline()
 
     return {
         '__default__':
@@ -28,5 +28,5 @@ def register_pipelines() -> Dict[str, Pipeline]:
         'data_processing': data_processing_pipeline,
         'data_science': data_science_pipeline,
         'inference': inference_pipeline,
-        'api_inference': api_inference_pipeline,
+        #'api_inference': api_inference_pipeline,
     }
